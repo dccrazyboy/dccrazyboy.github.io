a221444700b0c910de3b5037bfe9429bc1273875
董超 <dongchao@meituan.com>
2019-04-30 09:21:16 +0800

    comment DISQUS_SITENAME

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -36,4 +36,4 @@ PLUGINS = ["render_math"]
 THEME = "themes/pelican-blue"
 MENUITEMS = (('Home', '/'),)
 GOOGLE_ANALYTICS = "UA-38402381-7"
-DISQUS_SITENAME = "dongchao"
+# DISQUS_SITENAME = "dongchao"
